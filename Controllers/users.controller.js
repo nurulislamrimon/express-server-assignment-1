367ee7c30178a14b6bf1ab2cea79c1ccf12e0424
nurulislamrimon <nurulislamrimon@gmail.com>
2023-03-15 12:46:04 +0600

    update an user information with validation of id done

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -15,13 +15,12 @@ module.exports.getAllUser = (req, res) => {
 
     const usersData = userQuery ? users.slice(0, userQuery) : users;
     res.status(200).send({ users: usersData })
-
     console.log(`${userQuery} users responding`);
 }
 
 module.exports.postAnUser = (req, res) => {
     const { id, name, gender, photoUrl, contact, address } = req.body;
-    if (id && name && gender && photoUrl && contact && address) {
+    if ((id || id === 0) && name && gender && photoUrl && contact && address) {
         users.push(req.body);
         res.status(200).send({ result: "successfully new user added" })
         console.log("successfully new user added");
@@ -29,5 +28,23 @@ module.exports.postAnUser = (req, res) => {
     else {// console.log(newUser);
         console.log("Faild to add new user! Something is missing");
         res.status(400).send({ result: "Faild to add new user! Something is missing" })
+    }
+}
+
+module.exports.updateAnUser = (req, res) => {
+    const { id, name, gender, photoUrl, contact, address } = req.body;
+    if ((id || id === 0) && name && gender && photoUrl && contact && address) {
+        const existingUser = users.find(user => user.id === id);
+        if (existingUser) {
+            const indexOfUser = users.indexOf(existingUser);
+            users[indexOfUser] = req.body;
+            res.status(200).send({ result: "user information has been updated" })
+            console.log(`${indexOfUser} index user updated.`);
+        } else {
+            res.status(400).send({ result: `user id ${id} is not found` })
+        }
+    } else {
+        res.status(400).send({ result: "Faild to update user! Some information is missing!" });
+        console.log("Some information is missing!");
     }
 }
--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllUser, getRandomUser, postAnUser } = require('../Controllers/users.controller');
+const { getAllUser, getRandomUser, postAnUser, updateAnUser } = require('../Controllers/users.controller');
 
 const usersRouter = express.Router();
 
 usersRouter.route("/random").get(getRandomUser);
 usersRouter.route("/all").get(getAllUser);
-usersRouter.route("/save").post(postAnUser)
+usersRouter.route("/save").post(postAnUser);
+usersRouter.route("/update").patch(updateAnUser);
 
 module.exports = usersRouter;
